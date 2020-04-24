c9c7f8dc0b3290c05040896b91feccb11b854c64
Aaron Huggins <ahuggins@nethealthshops.com>
2020-04-24 10:14:54 -0500

    Enhance service class

--- service.js
+++ service.js
@@ -1,72 +1,95 @@
 const cp = require('child_process')
 const noop = () => {}
-const STATE = {
+const BIND_FUNCTIONS = ['start', 'stop', 'restart', 'kill']
+const SERVICE_STATE = {
   READY: 'READY',
   STARTED: 'STARTED',
   STOPPED: 'STOPPED',
-  RESTARTING: 'RESTARTING'
+  RESTARTING: 'RESTARTING',
+  UNDEFINED: 'UNDEFINED'
 }
-const EVENT = {
+const SERVICE_EVENT = {
   ONREADY: 'onReady',
   ONSTART: 'onStart',
   ONSTOP: 'onStop',
   ONRESTART: 'onRestart'
 }
-Object.seal(this.STATE)
-Object.seal(this.EVENT)
+Object.seal(SERVICE_STATE)
+Object.seal(SERVICE_EVENT)
 
 class Service {
-  constructor (command, args, options) {
-    this.command = command
-    this.args = args
-    this.options = { ...options }
-    this.pid = null
-    this.events = new Map([
-      [EVENT.ONREADY, noop],
-      [EVENT.ONSTART, noop],
-      [EVENT.ONSTOP, noop],
-      [EVENT.ONRESTART, noop]
-    ])
+  constructor (options) {
+    const { name, command, args } = options
 
-    for (let key of this.events.keys()) {
-      if (typeof this.options[key] === 'function') {
-        this.events.set(key, this.options[key])
-        delete this.options[key]
+    if (command === undefined || command === null) {
+      this.state = SERVICE_STATE.UNDEFINED
+      this.pid = null
+    } else {
+      this.pid = null
+      this.events = new Map([
+        [SERVICE_EVENT.ONREADY, noop],
+        [SERVICE_EVENT.ONSTART, noop],
+        [SERVICE_EVENT.ONSTOP, noop],
+        [SERVICE_EVENT.ONRESTART, noop]
+      ])
+      this.options = { ...options }
+      this.name = name || command
+      this.command = command
+      this.args = args
+
+      delete this.options.name
+      delete this.options.command
+      delete this.options.args
+
+      for (let key of this.events.keys()) {
+        if (typeof this.options[key] === 'function') {
+          this.events.set(key, this.options[key])
+          delete this.options[key]
+        }
       }
-    }
 
-    this.state = STATE.READY
-    this.events.get(EVENT.ONREADY)()
+      for (let func of BIND_FUNCTIONS) {
+        this[func] = this[func].bind(this)
+
+        Object.defineProperty(this[func], 'name', {
+          value: `${this.name || this.command} ${func}`
+        })
+      }
+
+      this.state = SERVICE_STATE.READY
+      this.events.get(SERVICE_EVENT.ONREADY)()
+    }
   }
 
   async start () {
-    this.events.get(EVENT.ONSTART)()
+    if (this.state === SERVICE_STATE.UNDEFINED) return
+    this.events.get(SERVICE_EVENT.ONSTART)()
     const spawn = cp.spawn(this.command, this.args, {
       stdio: 'ignore',
       ...this.options
     })
     spawn.unref()
     this.pid = spawn.pid
-    this.state = STATE.STARTED
-    return this
+    this.state = SERVICE_STATE.STARTED
   }
 
   async stop () {
+    if (this.state === SERVICE_STATE.UNDEFINED) return
     this.kill()
-    this.state = STATE.STOPPED
-    this.events.get(EVENT.ONSTOP)()
-    return this
+    this.state = SERVICE_STATE.STOPPED
+    this.events.get(SERVICE_EVENT.ONSTOP)()
   }
 
   async restart () {
-    this.state = STATE.RESTARTING
+    if (this.state === SERVICE_STATE.UNDEFINED) return
+    this.state = SERVICE_STATE.RESTARTING
     this.kill()
-    this.events.get(EVENT.ONRESTART)()
+    this.events.get(SERVICE_EVENT.ONRESTART)()
     await this.start()
-    return this
   }
 
   kill (signal = 'SIGINT') {
+    if (this.state === SERVICE_STATE.UNDEFINED) return
     if (this.pid !== null) {
       process.kill(this.pid, signal)
       this.pid = null
@@ -74,4 +97,4 @@ class Service {
   }
 }
 
-module.exports = { Service }
+module.exports = { Service, SERVICE_EVENT, SERVICE_STATE }
