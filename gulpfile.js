4ab94b053491986329a1262c7388484d5c86bc6b
Aaron Huggins <ahuggins@nethealthshops.com>
2020-04-24 10:15:08 -0500

    Migrate gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,36 +3,22 @@ const shell = require('gulp-shell')
 const fs = require('fs')
 const path = require('path')
 const { Service } = require('./service')
-let azurite = null
-
-gulp.task('azurite:start', async done => {
-  const azuriteDir = './.azurite'
-  const serviceOpts = {
-    onStop: () => fs.rmdirSync(azuriteDir, { recursive: true })
-  }
-  azurite = new Service(
-    'node',
-    [path.normalize('./node_modules/azurite/bin/table'), '-l', azuriteDir],
-    serviceOpts
-  )
-  await azurite.start()
-  done()
+const azuriteDir = './.azurite'
+const azurite = new Service({
+  name: 'azurite',
+  command: 'node',
+  args: [path.normalize('./node_modules/azurite/bin/table'), '-l', azuriteDir],
+  onStop: () => fs.rmdirSync(azuriteDir, { recursive: true })
 })
 
-gulp.task('azurite:stop', async done => {
-  if (azurite !== null) {
-    azurite.stop()
-    azurite = null
-  }
-  done()
-})
-
-gulp.task('mocha', shell.task(['mocha'], { ignoreErrors: true }))
-
-gulp.task('nyc', shell.task(['nyc mocha'], { ignoreErrors: true }))
-
-gulp.task('test', gulp.series('azurite:start', 'mocha', 'azurite:stop'))
-
-gulp.task('coverage', gulp.series('azurite:start', 'nyc', 'azurite:stop'))
-
-gulp.task('compile', shell.task(['tsc']))
+exports.mocha = async function mocha () {
+  return shell.task(['mocha'], { ignoreErrors: true })
+}
+exports.nyc = async function nyc () {
+  return shell.task(['nyc mocha'], { ignoreErrors: true })
+}
+exports.test = gulp.series(azurite.start, exports.mocha, azurite.stop)
+exports.coverage = gulp.series(azurite.start, exports.nyc, azurite.stop)
+exports.compile = async function compile () {
+  return shell.task(['tsc'])
+}
